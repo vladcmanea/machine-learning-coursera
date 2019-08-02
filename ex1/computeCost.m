959555c1d88783b70311db291af7ffc1873eb815
Vlad Manea <vlad.c.manea@gmail.com>
2019-08-02 20:38:47 +0200

    Adding exercise 1

---
+++ ex1/computeCost.m
@@ -0,0 +1,15 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+hsum = theta' * X';
+diff = hsum - y';
+sqrs = diff * diff';
+sum = sum(sqrs);
+J = sum / 2 / m;
+
+end
